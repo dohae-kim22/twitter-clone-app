575cea892db5f19e7f33c055286199e11902003c
DODO <dohae.kim22@gmail.com>
2025-07-17 11:03:29 +0200

    feat: Setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import CreateAccount from "./pages/CreateAccount";
+
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "profile",
+          element: <Profile />,
+        },
+      ],
+    },
+    { path: "/login", element: <Login /> },
+    { path: "/create-account", element: <CreateAccount /> },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
