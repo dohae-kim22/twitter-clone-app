39e05b7674837c16c2b58b2783a2e933412dd99b
DODO <dohae.kim22@gmail.com>
2025-07-17 15:34:50 +0200

    feat: Add login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,75 @@
-export default function Login () {
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import {
+  Title,
+  Wrapper,
+  Form,
+  Input,
+  Switcher,
+  Error,
+} from "../components/AuthComponents";
+
+export default function Login() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLoading || !email || !password) return;
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        setError(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <>
-      <h1>Login Component</h1>
-    </>
+    <Wrapper>
+      <Title>Log into 𝕏</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          placeholder="E-mail"
+          type="email"
+          onChange={handleChange}
+          value={email}
+          required
+        />
+        <Input
+          name="password"
+          placeholder="Password"
+          type="password"
+          onChange={handleChange}
+          value={password}
+          required
+        />
+        <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an account? <Link to="/create-account">Create one</Link>
+      </Switcher>
+    </Wrapper>
   );
 }
