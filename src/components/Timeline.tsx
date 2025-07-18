249d4be4754fde916b04d885f7d1fad4918f4a21
DODO <dohae.kim22@gmail.com>
2025-07-18 10:21:31 +0200

    feat: Fetch tweets in real-time using onSnapshot

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,4 +1,11 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  type Unsubscribe,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import Tweet from "./Tweet";
@@ -15,28 +22,38 @@ export interface ITweet {
 export default function Timeline() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
 
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    );
-
-    const snapshot = await getDocs(tweetsQuery);
-    const fetchedTweets = snapshot.docs.map((doc) => {
-      const { tweet, createdAt, photo, userId, userName } = doc.data();
-      return {
-        tweet,
-        createdAt,
-        photo,
-        userId,
-        userName,
-        id: doc.id,
-      };
-    });
-    setTweets(fetchedTweets);
-  };
-
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const fetchedTweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, photo, userId, userName } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            photo,
+            userId,
+            userName,
+            id: doc.id,
+          };
+        });
+        console.log(fetchTweets);
+        setTweets(fetchedTweets);
+      });
+
+      return () => {
+        if (unsubscribe) {
+          unsubscribe();
+        }
+      };
+    };
     fetchTweets();
   }, []);
   return (
